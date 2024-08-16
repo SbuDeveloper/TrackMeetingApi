a6c8a6193fec764cb52e4f20580c579b1e3aac07
SbuDeveloper <sbunyambose@gmail.com>
2024-08-16 09:30:37 +0200

    Updated gitIgnore

--- TrackMngmtMeetings/obj/Debug/net8.0/TrackMngmtMeetings.AssemblyInfo.cs
+++ TrackMngmtMeetings/obj/Debug/net8.0/TrackMngmtMeetings.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrackMngmtMeetings")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8532f4ce033994362596ec1a48680bcd0d5bac3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00daed5112d1e680ffd67d50e1db176f9b828eb6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrackMngmtMeetings")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrackMngmtMeetings")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
